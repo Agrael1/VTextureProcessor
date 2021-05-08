86a7be4096231962ceadbd7b6c5e494b21be739e
David Černý <cernyd@users.noreply.github.com>
2021-05-08 21:31:07 +0200

    FlowView documented

--- TextureProcessor/src/UI/FlowView.cpp
+++ TextureProcessor/src/UI/FlowView.cpp
@@ -1,5 +1,10 @@
 #include <UI/FlowView.h>
 
+/**
+ * @brief Construct a new Flow View:: Flow View object
+ *
+ * @param scene Scene to create View of
+ */
 FlowView::FlowView(UI::FlowScene& scene)
 	:scene(scene), QGraphicsView(&scene, nullptr), menu(nullptr), view_menu("View"),
 	delet("Delete selected"), clrselect("Clear Selection")
@@ -15,6 +20,7 @@ FlowView::FlowView(UI::FlowScene& scene)
 	setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
 	setCacheMode(QGraphicsView::CacheBackground);
 
+	// Create context menu
 	auto& m = scene.GetGroupList();
 	for (const auto& x : m)
 	{
@@ -25,14 +31,22 @@ FlowView::FlowView(UI::FlowScene& scene)
 	menu.Finish();
 	menu.SetItemClickCallback([this](QTreeWidgetItem* item, int) {OnItemSelected(item, 0); });
 
+	// Register shortcuts for object deletion (DEL) and selection clearing (ESC)
 	clrselect.setShortcut(Qt::Key::Key_Escape);
 	delet.setShortcut(Qt::Key::Key_Delete);
+
 	connect(&clrselect, &QAction::triggered, &scene, &UI::FlowScene::clearSelection);
 	connect(&delet, &QAction::triggered, &scene, &UI::FlowScene::DeleteSelected);
+
 	view_menu.addAction(&delet);
 	view_menu.addAction(&clrselect);
 }
 
+/**
+ * @brief Sets zoom level based on scroll event
+ *
+ * @param event Scroll event
+ */
 void FlowView::wheelEvent(QWheelEvent* event)
 {
 	QPoint delta = event->angleDelta();
@@ -50,6 +64,12 @@ void FlowView::wheelEvent(QWheelEvent* event)
 	else
 		scaleDown();
 }
+
+/**
+ * @brief Handles events in context menu
+ *
+ * @param event
+ */
 void FlowView::contextMenuEvent(QContextMenuEvent* event)
 {
 	last_event = event->pos();
@@ -65,6 +85,10 @@ void FlowView::contextMenuEvent(QContextMenuEvent* event)
 	menu.Execute(event->globalPos());
 }
 
+/**
+ * @brief Scales up view and checks for limit
+ *
+ */
 void FlowView::scaleUp()
 {
 	constexpr double factor = 1.2;
@@ -75,6 +99,11 @@ void FlowView::scaleUp()
 
 	scale(factor, factor);
 }
+
+/**
+ * @brief Scales down view and checks for limit
+ *
+ */
 void FlowView::scaleDown()
 {
 	constexpr double factor = 1 / 1.2;
@@ -84,17 +113,18 @@ void FlowView::scaleDown()
 	scale(factor, factor);
 }
 
+/**
+ * @brief Creates new Node of selected type when selected in context menu
+ *
+ * @param item
+ */
 void FlowView::OnItemSelected(QTreeWidgetItem* item, int)
 {
 	auto modelName = item->data(0, Qt::UserRole).toString().toStdString();
-
-	if (modelName == skipper)return;
+	if (modelName == skipper) return;
 
 	auto& type = scene.CreateNode(modelName);
-	{
-		QPointF posView = mapToScene(last_event);
-		type.setPos(posView);
-	}
+	type.setPos(mapToScene(last_view));
 
 	menu.close();
 }
